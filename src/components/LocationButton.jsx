5f4d7545329465ab0fcd9ad2bcfa285df4bcca6b
mongchongi <yeonguk0517@gmail.com>
2025-04-04 15:10:14 +0900

    도시별 버튼 리스트화

--- src/components/LocationButton.jsx
+++ src/components/LocationButton.jsx
@@ -1,11 +1,12 @@
-const LocationButton = () => {
+const LocationButton = ({ cities }) => {
   return (
     <div className='locations'>
       <button className='locations__button locations__button--current'>Current Location</button>
-      <button className='locations__button'>Japan</button>
-      <button className='locations__button'>Vietnam</button>
-      <button className='locations__button'>Paris</button>
-      <button className='locations__button'>New York</button>
+      {cities.map((city) => (
+        <button className='locations__button' key={city}>
+          {city}
+        </button>
+      ))}
     </div>
   );
 };
